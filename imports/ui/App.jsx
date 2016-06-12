6bd29b93613b8affabf7d4db554766d7258db572
John Wang <john29917958@gmail.com>
2016-06-12 22:59:04 +0800

    Add filter (react state) to App.jsx

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -7,6 +7,20 @@ import { Tasks } from '../api/tasks.js';
 import Task from './Task.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hideCompleted: false
+    };
+  }
+
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted
+    });
+  }
+
   handleSubmit(event) {
     event.preventDefault();
 
@@ -21,7 +35,13 @@ class App extends Component {
   }
 
   renderTasks() {
-    return this.props.tasks.map(function (task) {
+    var filteredTasks = this.props.tasks;
+
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+
+    return filteredTasks.map(function (task) {
       return <Task key={task._id} task={task} />
     });
   }
@@ -31,6 +51,11 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <label className="hide-completed">
+            <input type="checkbox" readOnly checked={ this.state.hideCompleted } onClick={ this.toggleHideCompleted.bind(this) } />
+            Hide Completed Tasks
+          </label>
 
           <form className="new-task" onSubmit={ this.handleSubmit.bind(this) }>
             <input type="text" ref="textInput" placeholder="Type to add new tasks" />
