8a9a3332b1c466fb1042f513aa2abc23cd088917
John Wang <john29917958@gmail.com>
2016-06-12 23:05:53 +0800

    Add incomplete count

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -50,7 +50,7 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({ this.props.incompleteCount })</h1>
 
           <label className="hide-completed">
             <input type="checkbox" readOnly checked={ this.state.hideCompleted } onClick={ this.toggleHideCompleted.bind(this) } />
@@ -71,11 +71,13 @@ class App extends Component {
 }
 
 App.propTypes = {
-  tasks: PropTypes.array.isRequired
+  tasks: PropTypes.array.isRequired,
+  incompleteCount: PropTypes.number.isRequired
 };
 
 export default createContainer(function () {
   return {
-    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch()
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count()
   };
 }, App);
